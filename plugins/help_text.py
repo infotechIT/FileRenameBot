e9d2c3c0471c935195ac611880e585ddc1236daa
InfoTech <infotechtele@gmail.com>
2022-04-12 21:34:17 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -102,12 +102,12 @@ async def start_me(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Channel', url='https://t.me/infotechbots'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/alxneboy')
                 ],
                 [
                     InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Source', url='https://github.com/infotechIT/FileRenameBot')
                 ]
             ]
         ),
